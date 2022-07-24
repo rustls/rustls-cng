cb65b3904539bc2db46920b898ceb4f4cf1d83c6
Dmitry Pankratov <dmitry@pankratov.net>
2022-07-24 16:22:36 +0200

    updated docs

--- doc/implementors/core/ops/drop/trait.Drop.js
+++ doc/implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["rustls_cng"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"rustls_cng/store/struct.CertStore.html\" title=\"struct rustls_cng::store::CertStore\">CertStore</a>","synthetic":false,"types":["rustls_cng::store::CertStore"]}];
+implementors["rustls_cng"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"rustls_cng/store/struct.CertStore.html\" title=\"struct rustls_cng::store::CertStore\">CertStore</a>","synthetic":false,"types":["rustls_cng::store::CertStore"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/source-files.js
+++ doc/source-files.js
@@ -1,3 +1,3 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["rustls_cng"] = {"name":"","files":["cert.rs","error.rs","key.rs","lib.rs","signer.rs","store.rs"]};
 createSourceSidebar();
