0375fdd6f978065219e12fdd7996311e0473fe3e
Dmitry Pankratov <dmitry@pankratov.net>
2023-09-05 08:17:42 +0200

    documentation link

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://ancwrd1.github.io/rustls-cng/doc/rustls_cng")]
+#![doc(html_root_url = "https://rustls.github.io/rustls-cng/doc/rustls_cng")]
 #![doc = include_str!("../README.md")]
 
 pub mod cert;
